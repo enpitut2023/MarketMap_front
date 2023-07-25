ab7a62f936ea7e540feb9cdfd500926476a58a5b
shin <gongtianzhen2021@126.com>
2023-07-25 12:14:35 +0900

    tab menu

--- src/DatabaseButtons.jsx
+++ src/DatabaseButtons.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Button } from "@mui/material";
 
-const DatabaseButtons = ({ database, addelem, category }) => {
+const DatabaseButtons = ({ database, addelem, category}) => {
 
   // データからボタンの生成
   const generateButtons = () => {
     return database.map((data) => {
       const buttonIndex = data[0]; 
-      const buttonCategory = data[1];
       const buttonText = data[2]; 
       const iconSrc = "/icon/" + data[5] + ".png";
+      const buttonCategory = data[1];
 
-      if (buttonCategory === category){
+      if(buttonCategory === category){
         return (
           <Button key={buttonIndex} variant="contained"  sx={{color: "white", background: "dimgray"}} onClick={() => addelem(buttonIndex, buttonText)} className="database-button">
             <img className="icon" src={iconSrc} alt="Icon" />
             <span>{buttonText}</span>
           </Button>
         );
-      };
+      }
     });
   };
 
